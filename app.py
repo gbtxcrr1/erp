85811cb005cca4a8795fb0ec99fe428a985af4fd
AnUnluckyGuyy <211049633+AnUnluckyGuyy@users.noreply.github.com>
2026-08-17 19:10:41 +0000

    add more windows

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import request, redirect, url_for
+from flask_migrate import Migrate
 
 from config import Config
 from models import db, Client
@@ -8,6 +9,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+
+# after adding new columns
+migrate = Migrate(app, db)
 
 @app.route("/")
 def home():
@@ -22,9 +26,24 @@ def clients():
 def new_client():
     if request.method == "POST":
         name = request.form["name"]
+        cpf = request.form["cpf"]
+        phone = request.form["phone"]
         email = request.form["email"]
+        address = request.form["address"]
+        city = request.form["city"]
+        state = request.form["state"]
+        cep = request.form["cep"]
 
-        new_client = Client(name=name, email=email)
+        new_client = Client(
+            name=name,
+            cpf=cpf,
+            phone=phone,
+            email=email,
+            address=address,
+            city=city,
+            state=state,
+            cep=cep
+        )
 
         db.session.add(new_client)
         db.session.commit()
@@ -36,6 +55,26 @@ def new_client():
 @app.route("/products")
 def products():
     return render_template("products.html")
+
+@app.route("/sales")
+def sales():
+    return render_template("sales.html")
+
+@app.route("/stock")
+def stock():
+    return render_template("stock.html")
+
+@app.route("/finance")
+def finance():
+    return render_template("finance.html")
+
+@app.route("/support")
+def support():
+    return render_template("support.html")
+
+@app.route("/employees")
+def employees():
+    return render_template("employees.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -5,7 +5,13 @@ db = SQLAlchemy()
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    phone = db.Column(db.String(12))
     email = db.Column(db.String(200), unique=True, nullable=False)
+    address = db.Column(db.String(200))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(2))
+    cep = db.Column(db.String(8))
 
     def __repr__(self):
         return f"<Cliente {self.name}>"
